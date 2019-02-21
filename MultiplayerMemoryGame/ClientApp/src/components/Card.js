ae7028f4279176a0623f57d35126f110155ed3d1
Ivan Ryakhov <ivryakhov@gmail.com>
2019-02-21 16:31:10 +0300

    Change cards styling

--- MultiplayerMemoryGame/ClientApp/src/components/Card.js
+++ MultiplayerMemoryGame/ClientApp/src/components/Card.js
@@ -4,13 +4,13 @@ import styles from './CardStyles';
 const Card = (props) => {
     const openedStyle = !props.close ? styles.opened : {};
     const disabledStyle = props.disabled ? styles.matched : {};
-    const backColor = props.matched ? 'has-background-warning' : 'has-background-success';
+    const backColor = props.matched ? 'has-background-warning grey-dark' : 'has-background-success has-text-light';
     return (
         <div style={{ ...styles.card, ...openedStyle, ...disabledStyle }} onClick={props.click} disabled={props.disabled}>
-            <div style={{ ...styles.div, ...styles.front }} className="has-background-primary has-text-white">
+            <div style={{ ...styles.div, ...styles.front }} className="has-background-primary has-text-light">
                 ?
             </div>
-            <div style={{ ...styles.div, ...styles.back }} className={`${backColor} has-text-white`}>
+            <div style={{ ...styles.div, ...styles.back }} className={backColor}>
                 <i className={`fas ${props.picture}`}/>
             </div>
         </div>
